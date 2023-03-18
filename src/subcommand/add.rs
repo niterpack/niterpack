b6cc7864ae26f32a77a45e4e9965ac8a8b9e471f
panda885 <firegox@email.cz>
2023-03-18 14:01:58 +0100

    Updated add command using #3

--- src/subcommand/add.rs
+++ src/subcommand/add.rs
@@ -1,34 +1,65 @@
 use std::env;
-use eyre::{eyre, WrapErr};
+use eyre::{ContextCompat, ensure, eyre, WrapErr};
 use log::info;
-use url::Url;
 use crate::format::ProjectFormatter;
 use crate::modrinth;
+use crate::modrinth::ModrinthProjectType;
 use crate::project::Mod;
 use crate::project::source::Source;
 
 #[derive(clap::Args)]
 pub struct AddArgs {
-    /// Source of the new mod, either url or modrinth version id
-    source: String,
+    /// Reference to a project to add as a mod
+    ///
+    /// You can reference a project using a slug, or an id.
+    #[arg(id = "MOD")]
+    mod_name: String,
 
-    /// Name of the new mod
-    #[arg(short, long)]
-    name: String
+    /// Reference to the project's version
+    ///
+    /// You can reference a version using a number, or an id.
+    #[arg(id = "VERSION", short = 'v', long = "version")]
+    version_name: Option<String>,
 }
 
 impl AddArgs {
-    pub fn parse_source(&self) -> eyre::Result<Source> {
-        if let Some(_) = modrinth::get_version(&self.source).wrap_err("failed to get modrinth version")? {
-            Ok(Source::Modrinth { version_id: self.source.clone() })
-        } else {
-            Url::parse(&self.source).map_err(|_| eyre!("invalid source url or modrinth version id"))?;
-            Ok(Source::Download { url: self.source.clone() })
-        }
-    }
-
     pub fn mod_data(&self) -> eyre::Result<Mod> {
-        Ok(Mod::new(self.name.clone(), None, self.parse_source()?))
+        let project = modrinth::get_project(&self.mod_name)
+            .wrap_err("failed to fetch modrinth project")?
+            .ok_or_else(|| eyre!("project `{}` not found", &self.mod_name))?;
+
+        ensure!(
+            project.project_type == ModrinthProjectType::Mod,
+            "only `mod` project types are allowed"
+        );
+
+        let version_id = match &self.version_name {
+            Some(version_name) =>
+                match modrinth::get_version(version_name).wrap_err("failed to fetch modrinth version")? {
+                    Some(version) => version.id,
+                    None => modrinth::get_versions(&self.mod_name)
+                        .wrap_err("failed to fetch modrinth project versions")?
+                        .into_iter()
+                        .find(|version| &version.version_number == version_name)
+                        .ok_or_else(|| eyre!("could not find version `{}`", version_name))?
+                        .clone()
+                        .id
+                },
+            None => project.versions
+                .last()
+                .wrap_err("project doesn't have any versions")?
+                .clone()
+        };
+
+        Ok(
+            Mod::new(
+                project.slug,
+                None,
+                Source::Modrinth {
+                    version_id
+                }
+            )
+        )
     }
 
     pub fn run(&self) -> eyre::Result<()> {
@@ -40,7 +71,7 @@ impl AddArgs {
 
         formatter.create_mod(&mod_data)?;
 
-        info!("Added mod `{}` to modpack", self.name);
+        info!("Added mod `{}` to modpack", mod_data.name);
         Ok(())
     }
 }
