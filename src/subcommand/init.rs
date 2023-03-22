04703bd349a8ffcf718546bc8780bddb0d05ab58
panda885 <firegox@email.cz>
2023-03-22 17:06:25 +0100

    Added init command

---
+++ src/subcommand/init.rs
@@ -0,0 +1,26 @@
+use std::env;
+use eyre::ContextCompat;
+use log::info;
+use crate::project::Project;
+
+#[derive(clap::Args)]
+pub struct InitArgs;
+
+impl InitArgs {
+    pub fn run(&self) -> eyre::Result<()> {
+        let current_dir = env::current_dir().unwrap();
+
+        let project = Project::new(
+            current_dir
+                .file_name()
+                .and_then(|name| name.to_os_string().into_string().ok())
+                .wrap_err("failed to get file name of the current directory")?,
+            String::from("0.1.0")
+        );
+
+        project.create(current_dir)?;
+
+        info!("Created a new modpack `{}`", &project.name);
+        Ok(())
+    }
+}
--- src/subcommand/mod.rs
+++ src/subcommand/mod.rs
@@ -1,8 +1,12 @@
 mod add;
 mod build;
+mod init;
 
 #[derive(clap::Subcommand)]
 pub enum Subcommand {
+    /// Create a new modpack in the current directory
+    Init(init::InitArgs),
+
     /// Add a new mod to the current modpack
     Add(add::AddArgs),
 
@@ -13,6 +17,7 @@ pub enum Subcommand {
 impl Subcommand {
     pub fn run(&self) -> eyre::Result<()> {
         match &self {
+            Subcommand::Init(args) => args.run(),
             Subcommand::Add(args) => args.run(),
             Subcommand::Build(args) => args.run()
         }
